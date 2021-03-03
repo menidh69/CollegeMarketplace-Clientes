e7554dec5d873f0a8aa9fc7497228853699769b2
paolaqgs <pao_quihui@hotmail.com>
2021-03-02 18:17:51 -0700

    login

--- App.js
+++ App.js
@@ -1,14 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View, TouchableOpacity, } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Bienvenido a College Marketplace para clientes!</Text>
-      <Text>OH YEAH</Text>
+      <Text>OH YEAH aqui va el login</Text>
       <StatusBar style="auto" />
+        <TouchableOpacity style={styles.loginBtn}>
+          <Text style={styles.loginText}>LOGIN</Text>
+        </TouchableOpacity>
     </View>
+
+
+
+
   );
 }
 
@@ -18,5 +24,14 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  loginBtn: {
+    width: "80%",
+    borderRadius: 25,
+    height: 50,
+    alignItems: "center",
+    justifyContent: "center",
+    marginTop: 40,
+    backgroundColor: "#FF1493",
   },
 });
